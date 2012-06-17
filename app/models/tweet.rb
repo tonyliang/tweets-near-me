efec0e99329cc0dba5eeca49b926ec8384388fec
tonyliang <chunghau.liang@gmail.com>
2012-06-16 17:26:35 -0700

    mongohq

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,5 +6,5 @@ class Tweet
   field :name, type: String
   field :text, type: String
   index({location:'2d'},{sparse:true})
-  store_in collection: 'tweets',database:'Banjo'
+  store_in collection: 'tweets',database:'app5314009'
 end
